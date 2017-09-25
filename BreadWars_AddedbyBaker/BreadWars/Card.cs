ae348a8fa0ce2909815965e0d3b88656a21b4584
keh1217 <keh1217@rit.edu>
2017-09-25 17:22:41 -0400

    added some attributes to Card.cs

--- BreadWars_AddedbyBaker/BreadWars/Card.cs
+++ BreadWars_AddedbyBaker/BreadWars/Card.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace BreadWars
 {
     class Card
     {
-
+        const String IMAGE_NAME = "";
         //atributes & properties
 
         //name
+        private String name;
         //value
+        private int value;
         //effect
+
         //special (effect active)
-        //image (?)
+
+        //image
+        
         //isBurned
+        private bool isBurned;
         //is8
+        private bool is8;
+
+        public String ImageName
+        {
+            get
+            {
+                return IMAGE_NAME;
+            }
+        }
 
     }
 }
