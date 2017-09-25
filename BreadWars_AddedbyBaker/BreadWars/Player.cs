bc306aa12d223c80e18a60c74c7270b607f749ec
keh1217 <keh1217@rit.edu>
2017-09-25 17:14:35 -0400

    Added initial Player Attributes

--- BreadWars_AddedbyBaker/BreadWars/Player.cs
+++ BreadWars_AddedbyBaker/BreadWars/Player.cs
@@ -8,9 +8,15 @@ namespace BreadWars
 {
     class Player
     {
+        const int PLAYER_MAX_HEALTH = 100;
         //hand (cards or slots) an array?
+        private Card[] hand = new Card[5];
         //health
+        private int playerCurrentHealth = PLAYER_MAX_HEALTH;
         //states (hiding cards on top of screen or showing cards on bottom of screen)
+        private bool handIsShowing;
         //status (poison and block)
+        private bool isPoisoned;
+        private bool hasBlock;
     }
 }
