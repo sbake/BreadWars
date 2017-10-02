cad6eb7ad18ea38102e1748bd92ac02db657a433
sxb2218 <sxb2218@IGMOH1370-01>
2017-10-02 17:28:33 -0400

    Small changes to Drawable, committing to pull.

--- BreadWars_AddedbyBaker/BreadWars/Drawable.cs
+++ BreadWars_AddedbyBaker/BreadWars/Drawable.cs
@@ -16,15 +16,24 @@ namespace BreadWars
         //Texture or spritesheet, or multiple textures
         //            cards will add another texture for back
         //            specials will have multiple animated things associated with them, ie. a special texture
+        //            spritesheets
         //position
-        
+        Texture2D texr; //single texture or spritesheet
+        Rectangle posit;
+
 
         //constructor
         //every drawable object needsa texture to be drawn, and a position to draw that texture
+        Drawable(Texture2D pTexr, Rectangle pPosit)
+        {
+            texr = pTexr;
+            posit = pPosit;
+        }
 
         //methods
         //method for drawing static objects (position doesn't move, can use variable)
+        //methods to unpack sprites
         //method for animated objects a (takes position as a parameter)
-        //method for animated objects b (moves through multiple textures)
+        //method for animated objects b (moves through multiple sprites)
     }
 }
