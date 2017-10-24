407ba2d08d1f995aef6f08f938793733d96e4771
Rebecca Medina (RIT Student) <rcm3517@ad.rit.edu>
2017-10-24 14:45:46 -0400

    self destruct
    change banker to self destruct

---
+++ BreadWars_AddedbyBaker/BreadWars/Special Cards/SelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BreadWars
+{
+    /// <summary>
+    /// Summary description for Banker
+    /// </summary>
+    public class SelfDestruct : Card
+
+    {
+        public SelfDestruct(Texture2D pTexr, Rectangle pPosit, bool active, Drawable pNumbers)  : base( pTexr,  pPosit,  active,  pNumbers) 
+        {
+            Name = "Self Destruct";
+            value = 17;
+            description = "You will have 3 turns left";
+        }
+    }
+}
