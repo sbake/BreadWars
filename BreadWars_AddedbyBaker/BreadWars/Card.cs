c15f5d17c78eec5d1a2bcc870173312a863b626c
sbake <sxb2218@rit.edu>
2017-11-06 17:40:33 -0500

    Goodbye flagpoles

--- BreadWars_AddedbyBaker/BreadWars/Card.cs
+++ BreadWars_AddedbyBaker/BreadWars/Card.cs
@@ -93,7 +93,7 @@ namespace BreadWars
                 //draw special if special
                 if (isActive)
                 {
-                    spriteBatch.Draw(texr, new Rectangle(posit.X, Posit.Y, Posit.Width, posit.Height*2), new Rectangle(spriteLocations[1], new Point(posit.Width/2, posit.Height)), Color.Red);
+                    spriteBatch.Draw(texr, new Rectangle(posit.X, Posit.Y, Posit.Width, posit.Height*2), new Rectangle(new Point(SpriteLocations[1].X +2, SpriteLocations[1].Y), new Point(posit.Width/2, posit.Height)), Color.Red);
                 }
                 //draw numbr
                 //int offsetFromCorners = 30; //add to posit
