f3d3ce7d8478aaba5e3fc3815a7b902019d4566e
Kyle Heide <music101piano@gmail.com>
2017-11-29 15:39:54 -0500

    ahhhhhhhhhhhhhhhh

--- BreadWars_AddedbyBaker/BreadWars/Game1.cs
+++ BreadWars_AddedbyBaker/BreadWars/Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System.IO;
+using Microsoft.Xna.Framework.Media;
 
 namespace BreadWars
 {
@@ -121,6 +122,9 @@ namespace BreadWars
         private MouseState mState;
         private MouseState mStatePrev;
         private bool resultCalculated;
+
+        //musique!
+
 
         public Game1()
         {
