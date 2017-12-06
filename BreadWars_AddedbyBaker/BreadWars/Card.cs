c7cacd24bd2993191a21c25bfb065cd9e64cf4f6
sbake <sbaker5234@gmail.com>
2017-12-06 17:18:33 -0500

    Card corrections (still not drawing correctly)

--- BreadWars_AddedbyBaker/BreadWars/Card.cs
+++ BreadWars_AddedbyBaker/BreadWars/Card.cs
@@ -132,7 +132,7 @@ namespace BreadWars
                 //draw special if special
                 if (isActive)
                 {
-                    spriteBatch.Draw(texr, new Rectangle(posit.X, posit.Y, posit.Width, posit.Height), new Rectangle(new Point(SpriteLocations[1].X, SpriteLocations[1].Y), new Point(posit.Width, posit.Height)), Color.White);
+                    spriteBatch.Draw(texr, posit, new Rectangle(new Point(SpriteLocations[1].X, SpriteLocations[1].Y), new Point(posit.Width, posit.Height)), Color.White);
                 }
                 //draw numbr
                 //int offsetFromCorners = 30; //add to posit
--- BreadWars_AddedbyBaker/BreadWars/Deck.cs
+++ BreadWars_AddedbyBaker/BreadWars/Deck.cs
@@ -20,7 +20,7 @@ namespace BreadWars
 
         //can change however you want later, just for testing if other stuffs work
         Texture2D[] cardTexts;
-        Rectangle cardSizeOnly = new Rectangle(0, 0, 250, 166);
+        Rectangle cardSizeOnly = new Rectangle(0, 0, 332, 250);
         Drawable numbers;
 
         public Deck(Texture2D[] pCardTexts, Drawable pNumbers)
